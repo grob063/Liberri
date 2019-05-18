d1030ff2e307323c3f18ec9723600a4c2f61b1f0
ardella <ardella.shelton@gmail.com>
2019-05-18 11:16:24 -0400

    5.18

---
+++ LibraryLists/BookList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryLists
+{
+    class BookList
+    {
+        public BookList()
+        {
+            List<Books> entry = new List<Books>();
+            {
+                new Books { Author = "Harper Lee", Title = "To Kill a MockingBird", CheckedOut = true };
+                new Books { Author = "Ray Bradbury", Title = "The Illustrated Man", CheckedOut = false };
+            }
+
+        }
+    }
+}
---
+++ LibraryLists/BookType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryLists
+{
+    public enum BookType
+    {
+        To_Kill_a_Mockingbird = 0,
+
+        The_Illustrated_Man = 1,
+    }
+}
