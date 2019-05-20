73b81ef4ca8ca86c54e08d21208b38ff3eb40699
Adam <lottadam@umich.edu>
2019-05-20 19:15:29 -0400

    added file I/O functionality

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Hello
 {
@@ -6,7 +8,61 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What up yo!");
+            var parsedBookData = GetFileMetadata();
+            var bookList = BuildBookInstanceList(parsedBookData);
+
+            
+            foreach (var book in bookList)
+            {
+                Console.WriteLine(book.Author);
+                Console.WriteLine(book.Name);
+                Console.WriteLine(book.Status);
+                Console.WriteLine(book.Date);
+            }
+            
+
+            Console.Write("Enter a new author: ");
+            bookList[0].Author = Console.ReadLine();
+
+            BuildBookPropertyFile(bookList);
+
+        }
+
+        public static List<string[]> GetFileMetadata()
+        {
+            var parsedBookData = new List<string[]>();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Shusui\desktop\books.txt");
+            foreach (var line in lines)
+            {
+                parsedBookData.Add(line.Split(","));
+            }
+            return parsedBookData;
+        }
+
+        public static List<Book> BuildBookInstanceList(List<string[]> fileMetadata)
+        {
+            var bookList = new List<Book>();
+            foreach (var item in fileMetadata)
+            {
+                var bookBuilder = new BookBuilder();
+                bookList.Add(bookBuilder.AddAuthor(item[0])
+                    .AddTitle(item[1])
+                    .AddStatus(item[2])
+                    .AddDueDate(item[3])
+                    .Build());
+            }
+            return bookList;
+        }
+
+        public static void BuildBookPropertyFile(List<Book> bookList)
+        {
+            var bookPropList = new List<string>();
+            foreach (var book in bookList)
+            {
+                StringBuilder sb = new StringBuilder($"{ book.Author },{book.Name},{book.Status},{book.Date}");
+                bookPropList.Add(sb.ToString());
+            }
+            System.IO.File.WriteAllLines(@"C:\Users\Shusui\desktop\books.txt", bookPropList);
         }
     }
 }
